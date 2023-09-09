3c935c645e681b33f0c2d35f7700543363420c8a
XMax-Alex <478618860@qq.com>
2023-09-10 07:57:43 +0800

    hardhat-foundry-4naly3er

---
+++ sdk/src/client/index.ts
@@ -0,0 +1,3 @@
+export * from './ITokenClient';
+
+export * from './TokenClient';
---
+++ src/Token.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract Token is ERC20("Token", "TOK", 18) {}
